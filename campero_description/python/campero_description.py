f0645fc88cd77d5d1465474bc6cba5ef396b9197
jean.laneurit <jean.laneurit@inrae.fr>
2022-11-15 15:30:08 +0100

    description : add python executable able to create campero urdf

---
+++ campero_description/python/campero_description.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+import xacro
+
+from ament_index_python.packages import get_package_share_directory
+
+def urdf(prefix, mode, robot_model, controller_conf_yaml_file):
+
+    xacro_file = (
+        get_package_share_directory("campero_description")
+        + "/urdf/campero_"
+        + robot_model
+        + ".urdf.xacro"
+    )
+
+    urdf_xml = xacro.process_file(
+        xacro_file,
+        mappings={
+            "prefix": prefix,
+            "mode": mode,
+            "controller_conf_yaml_file": controller_conf_yaml_file,
+        },
+    )
+
+    return urdf_xml.toprettyxml()
