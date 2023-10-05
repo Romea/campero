70098671ff37fba210816a9774850488e9511839
Jean Laneurit <jean.laneurit@inrae.fr>
2023-10-05 13:59:28 +0200

    fix xacro error
    Signed-off-by: Jean Laneurit <jean.laneurit@inrae.fr>

--- campero_bringup/launch/campero_test.launch.py
+++ campero_bringup/launch/campero_test.launch.py
@@ -177,7 +177,8 @@ def generate_launch_description():
             " base_name:base",
             " mode:",
             LaunchConfiguration("mode"),
-        ]
+        ],
+        on_stderr="ignore",
     )
 
     declared_arguments.append(
